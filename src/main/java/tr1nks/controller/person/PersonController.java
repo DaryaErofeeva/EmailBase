3830a2abb04ac79bc7fc8334bb3e4b015fa3032e
Tr1nks <Tr1nks96@gmail.com>
2018-09-11 22:22:46 +0300

    templates

--- src/main/java/tr1nks/controller/person/PersonController.java
+++ src/main/java/tr1nks/controller/person/PersonController.java
@@ -5,4 +5,8 @@ import tr1nks.controller.Controller;
 public interface PersonController extends Controller {
     String VIEW_BASE = "person/";
     String URL_BASE = "/person/";
+
+    String STUDENT_PAGE_URL = URL_BASE + "student";
+    String STUDENT_VIEW_NAME = VIEW_BASE + "student";
+    String STUDENT_FILTER_MODEL_NAME = "studentFilterSetModel";
 }
