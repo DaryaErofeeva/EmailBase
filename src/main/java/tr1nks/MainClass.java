04546b956936252065bdef0d977c937f5f403e5b
Darya Yerofieieva <da.erofeeva@gmail.com>
2018-12-02 21:52:57 +0200

    Add liquibase

--- src/main/java/tr1nks/MainClass.java
+++ src/main/java/tr1nks/MainClass.java
@@ -10,25 +10,9 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
-@ComponentScan({
-        "tr1nks.component",
-        "tr1nks.config",
-        "tr1nks.controller",
-        "tr1nks.service",
-})
 public class MainClass extends SpringBootServletInitializer {
-    public static final Class[] classes = {MainClass.class,};
-    public static ApplicationContext ac;
 
     public static void main(String[] args) {
-        System.setProperty("spring.devtools.restart.enabled", "true");//todo remove in prod
-        System.setProperty("spring.thymeleaf.cache", "false");
-        ac = SpringApplication.run(classes, args);
-    }
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(classes);
+        SpringApplication.run(MainClass.class, args);
     }
 }
